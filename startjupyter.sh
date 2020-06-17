360c14b6a7812b30c06a0d6533b7e22918586ecc
Nikolas Garofil <nikolas.garofil@uantwerpen.be>
2020-06-17 10:09:20 +0200

    Betere uitvoer in startjupyter.sh

--- startjupyter.sh
+++ startjupyter.sh
@@ -8,9 +8,11 @@ NAAM=beurs	#pas dit aan als ge al een container met de naam beurs hebt
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 GID=$(id -g $USER)
-docker stop beurs 2> /dev/null
+docker stop beurs > /dev/null 2> /dev/null
 echo "Mogelijks gaat hieronder wat docker uitvoer komen als de image nog gedownload worden. Dit is geen probleem maar kan even duren"
-echo "Er gaat ook altijd eeh hash als uitvoer gegeven worden (de hash van de containernaam)"
+echo "Er gaat ook altijd eeh hash als uitvoer gegeven worden (de hash van de containernaam):"
 docker run --user root -w /home/$USER -e CHOWN_HOME=yes -e NB_UID=$UID -e NB_GID=$GID -e NB_USER=$USER -d --rm -p $EXTERNEPOORT:8888 -v $DIR/bestanden:/home/$USER --name $NAAM jupyter/scipy-notebook:dd2087c75645
+echo ""
+echo "Gebruik:"
 echo "-  Login op http://localhost:8888 met wachtwoord 'pass'"
 echo "-  Draai 'docker stop $NAAM' of gebruik de 'quit' knop in jupyter om af te sluiten"
